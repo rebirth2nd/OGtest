7a5eb97b6ad7e0e55929c0981ead1beb9cce90ba
qingyuan <qingyuanzhang@email.arizona.edu>
2014-03-07 19:44:22 -0500

    customize admin page with minor changes

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from shop.models import Store, Product, Manufacturer
+
+# Admin classes
+
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ('name', 'store', "dollar_price", "manufacturer", "description")
+  search_fields = ['name']
+
+class ProductInline(admin.StackedInline):
+  model = Product
+  extra = 0
+
+class StoreAdmin(admin.ModelAdmin):
+  inlines = [ProductInline]
 
 # Register your models here.
+
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Manufacturer)
+
